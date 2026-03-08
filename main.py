c8e8914ae6b2a1d560ce4cebd9690a13d50e8c27
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-03-07 20:21:47 -0600

    feat: update readme.md and main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def flatten(arr):
+    arr_flat = []
+    for item in arr:
+        if isinstance(item, list):
+            flattern(item)
+        else:
+            arr_flat += item
+
+    return arr_flat
+
+if __name__ == '__main__':
+    print(flatten([1, [2, 3], 4]))
+    print('=======')
+    print(flatten([5, [4, [3, 2]], 1]))
+    print('=======')
+    print(flatten(["A", [[[["B"]]]], "C"]))
+    print('=======')
+    print(flatten([["L", "M", "N"], ["O", ["P", "Q", ["R", ["S", ["T", "U"]]]]], "V", ["W", ["X", ["Y", ["Z"]]]]]))
+    print('=======')
+    print(flatten([["red", ["blue", ["green", ["yellow", ["purple"]]]]], "orange", ["pink", ["brown"]]]))
